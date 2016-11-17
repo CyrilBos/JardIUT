83e05ded47c18e3e6480e7d3716e816d301827cf
jucrs <cressonjulie@gmail.com>
2016-11-17 15:37:35 +0100

    Mise a jour code arduino

---
+++ Arduino/humidite/humidite.ino
@@ -0,0 +1,55 @@
+class Sensor {
+  public:
+    int output_port; //port to send the data to
+    int low_threshold; 
+    int high_threshold;
+};
+
+const int PUMP_PIN = 48;
+
+Sensor humidity;
+
+String input;
+
+int mode;
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(PUMP_PIN, OUTPUT);
+    humidity.output_port = 1;
+    humidity.low_threshold = 280;
+    humidity.high_threshold = 490;
+}
+
+void active_pompe() {
+    digitalWrite(PUMP_PIN, HIGH);
+}
+
+void desactive_pompe() {
+    digitalWrite(PUMP_PIN, LOW);
+}
+
+
+void loop() {
+  //String input = Serial.readString();
+  input = "0";
+  if (input == "0") {
+    mode = 0;
+  }  
+  switch(mode) {
+    case 0:
+      break;
+    case 1:
+        Serial.println(analogRead(humidity.output_port));
+    
+        if (analogRead(humidity.output_port) < humidity.low_threshold) {
+            active_pompe();
+        }
+    
+        else if (analogRead(humidity.output_port) > humidity.high_threshold) {
+            desactive_pompe();
+        }
+        break;
+  }
+  delay(1000);
+}
